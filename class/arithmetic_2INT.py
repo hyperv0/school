80a01008f3c345dcb9be3f1d1fd6ed07367b241b
hyperv0 <maanyapatel0@gmail.com>
2024-07-08 11:22:00 +0530

    class

---
+++ class/arithmetic_2INT.py
@@ -0,0 +1,17 @@
+# WAP to enter two integers and perform all arithmetic operations
+a = int(input("Enter the 1st number:  "))
+b = int(input("Enter the 2nd number:  "))
+
+addition = a+b
+difference = a-b
+float_division = a/b
+int_division = a//b
+multi = a*b
+mod = a%b
+
+print("The addition of two numbers is:  ", addition)
+print("The difference of two numbers is:  ", difference)
+print("The float division of two numbers is:  ", float_division)
+print("The int division of two numbers is:  ", int_division)
+print("The multi of two numbers is:  ", multi)
+print("The mod of two numbers is:  ", mod)
---
+++ class/km_to_miles.py
@@ -0,0 +1,6 @@
+#WAP to input value in kilometer and convert it into miles ( 1 km = 0.621 miles)
+
+km = int(input("Enter the number of kilemetres:  "))
+miles = km*0.621
+
+print("Number of miles: ", miles)
