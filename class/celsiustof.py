96c968f4fe08a9730f93803330ecdc175e13122c
hyperv0 <maanyapatel0@gmail.com>
2024-07-08 11:23:05 +0530

    class

---
+++ class/celsiustof.py
@@ -0,0 +1,6 @@
+#WAP to convert temp from celsius to fairenheit
+
+c = int(input("Enter the temperature in celsius:  ")) 
+fahreinheit = (9/5*c) + 32
+
+print("The temperature in fahreinheit is ", fahreinheit)
