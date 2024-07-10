7f34f160812491fa2cbd9082274598d014b689f6
hyperv0 <maanyapatel0@gmail.com>
2024-07-10 10:50:56 +0530

    class

---
+++ class/a.py
@@ -0,0 +1,11 @@
+#String
+
+a = '''This is a
+multiline
+string'''
+print(a)
+
+
+s = "   "
+print(s)
+print(len(s))
---
+++ class/index.py
@@ -0,0 +1,9 @@
+s = "hello world"
+print(s[:6] + s[6:])
+
+
+for i in s:
+    print(i, end="   ")
+print(s+s)
+
+print("hello" in s)
