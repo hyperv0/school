5c0e308c8b5a3b11657b4ce33a6bfb951597078c
hyperv0 <maanyapatel0@gmail.com>
2024-07-11 10:31:03 +0530

    class

---
+++ class/b.py
@@ -0,0 +1,8 @@
+#WAP to input two strings, if string 1 contains string 2 then create a third string with first four characters of string 2 added with word 'restore'
+
+string1 = input("String 1: ")
+string2 = input("String 2: ")
+
+if string1 in string2:
+    string3 = string2[0:4] + "restore"
+    print(string3)
