1e3728306de03544fc79c1b73dc182412cb935ed
hyperv0 <maanyapatel0@gmail.com>
2024-07-18 10:37:05 +0530

    class

---
+++ journal/replace_space.py
@@ -0,0 +1,4 @@
+#WAP that takes a sentences as an input where each word is seperated by space. Use the function 'replace' to replace each space with a hyphen and then print the sentence.
+
+s = input('Enter the string: ')
+print(s.replace(" ", "_"))
