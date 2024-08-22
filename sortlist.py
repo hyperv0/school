0110fd38d889ab5978dd65371c40c75ba07d6560
hyperv0 <maanyapatel0@gmail.com>
2024-08-22 15:40:34 +0000

    Thu, Aug 22, 2024, 9:10 PM +05:30

---
+++ sortlist.py
@@ -0,0 +1,3 @@
+#WAP to read a list of n integers (positive as well as negative) create two new lists one having all postive no. and another having all negative numbers
+
+lst = [1,43,2,3,-4,6,3-2,-2,4]
