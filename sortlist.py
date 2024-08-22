3b18aa6822c2931936281bf6a1caa374220c1fdb
hyperv0 <maanyapatel0@gmail.com>
2024-08-22 15:42:52 +0000

    Thu, Aug 22, 2024, 9:12 PM +05:30

--- sortlist.py
+++ sortlist.py
@@ -1,3 +1,6 @@
 #WAP to read a list of n integers (positive as well as negative) create two new lists one having all postive no. and another having all negative numbers
 
-lst = [1,43,2,3,-4,6,3-2,-2,4]
+lst = [-1,43,2,3,-4,6,3,-45,-9,4]
+
+def sep(lst):
+    positive = []
