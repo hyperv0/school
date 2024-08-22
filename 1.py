180c51a32dc8cb9f9aa670acf600178927a0d997
hyperv0 <maanyapatel0@gmail.com>
2024-08-22 10:37:47 +0530

    class

--- 1.py
+++ 1.py
@@ -1,6 +1,10 @@
 #WAP that takes sentence a an input where each word in a sentence seperated by a space the function should replace each blank with a hypen and then print the sentence
 
-a = input('Enter the sentence: ')
+def main():
+    a = input('Enter the sentence: ')
+    print(replace(a))
 
 def replace(x: str):
     return x.replace(" ", '-')
+
+main()
--- 2.py
+++ 2.py
@@ -1,2 +1,4 @@
 #WAP that input a list, replicates it twice and then prints the sorted list in ascending order 
 #WAP to read a list of n integers (positive as well as negative) create two new lists one having all postive no. and another having all negative numbers  
+#WAP that reads a string and check whether it is a palandrome string or not
+#in a school fest three randomly choosen students out of 100 students( having roll no. 1 to 100 ) have to present bouquets to guest  help the school authorities choose three students randomly
