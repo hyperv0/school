a4162acb601f221f94180474a6361281addc84d5
hyperv0 <maanyapatel0@gmail.com>
2024-08-29 10:51:25 +0530

    class

---
+++ t.py
@@ -0,0 +1,9 @@
+#WAP to create three tuples 
+
+t1 = tuple(eval(input('Enter the input 1: ')))
+t2 = tuple(eval(input('Enter the input 2: ')))
+t3 = tuple(eval(input('Enter the input 3: ')))
+
+print(t1)
+print(t2)
+print(t3)
