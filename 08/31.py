49306123a11946d482403abf53f81e1ed3defd76
hyperv0 <maanyapatel0@gmail.com>
2024-08-31 08:31:21 +0530

    class

---
+++ 08/31.py
@@ -0,0 +1,24 @@
+#What is the output of the following code
+
+a,b = bool(0), bool(0.0)
+c,d = str(0), str(0.0)
+
+#print(len(a), len(b)) 
+print(len(c), len(d)) 
+
+tp1 = (15,11,17,16,12)
+#tp1.pop(12)
+#print(tp1)
+
+tp = (5,)
+tp1 = tp * 2
+print(len(tp1))
+
+plane = ('passangers', 'luggage')
+plane = list(plane)
+plane[1] = 'Electronic Items'
+print(plane)
+
+t = (1, (2,3,4,5), (6,7,8), 9)
+#print(sum(t))
+
