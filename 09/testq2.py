5c365c3e775c47efdd2ad121cc18a3457c39401e
hyperv0 <maanyapatel0@gmail.com>
2024-09-06 10:41:07 +0530

    class

---
+++ 09/testq2.py
@@ -0,0 +1,8 @@
+n1 = int(input('Enter the first integer: '))
+n2 = int(input('Enter the second integer: '))
+
+if n1 % n2 == 0:
+    print('Fully Divisible!')
+else:
+    print('Not fully divisible!')
+    
