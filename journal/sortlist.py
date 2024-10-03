e34553a8fabed974a8bc3c02d7b1fe9fda4b6a7b
hyperv0 <maanyapatel0@gmail.com>
2024-10-03 10:56:15 +0530

    prac

---
+++ journal/sortlist.py
@@ -0,0 +1,15 @@
+#WAP to read a list of n integers (positive as well as negative) create two new lists one having all postive no. and another having all negative numbers  
+
+lst = [-1,2,-3,4,-5,6,7,-8]
+postive = []
+negative = []
+
+for n in list:
+    if n > 0:
+        postive.append(n)
+    else:
+        negative.append(n)
+
+    
+print(positive)
+print(negative)
