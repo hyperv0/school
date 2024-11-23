9672e2bd467357648134ff89110d556993bf6cd1
hyperv0 <maanyapatel0@gmail.com>
2024-11-23 08:16:27 +0530

    class

---
+++ 11/30.py
@@ -0,0 +1,17 @@
+'''
+#WAP to accept and store employee name as a key and his salary, allowance and deduction as value in a dictionary(dict) for 10 employees.
+Display the following items:-
+
+a. Display total salary
+b. Display total allowance and deductions
+c. Search an Employee
+- When user selects option 1, diplay a report having Name,Salary, allowance , deduction, gross salary (salary + allowance) and net salary(gross - deductions)
+- When user selects option 2, find total allowance and deductions of all 10 employees.
+- When user selects option 3, accept name of the employee and search in the dictionary.
+'''
+
+
+
+
+
+
